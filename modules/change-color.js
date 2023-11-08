32ba04d922e4199f8972adcb6ac012038c97036a
daniel <juandanielpuchernandez@gmail.com>
2023-11-07 19:38:22 -0500

    change-color
    change-color hh

--- modules/change-color.js
+++ modules/change-color.js
@@ -1,15 +1,24 @@
 
-function changeColor(/* carta */, /* colores */) {
+function changeColor(card,colors) {
+    console.log("Carta actual:", card);
+    console.log("colores:", colors);
     // Recibir carta
     // preguntarle al usuario que color escoger
+    const selectedColor = rl.question("escoje un color: ", {
+        limit: colors,
+    limitMessage: "Selección de color no válida. Elige entre los colores disponibles.",
+  });
+  card.color = selectedColor;
     // cambiar el color 
     // Regresar la carta con la actualizacion 
     // del color seleccionado
-}
 // Modificar este codigo para que reciba el arreglo de colores y 
 // los muestre como opciones
 // el color seleccionado debe ser el nuevo valor para 
 // card.color, regresar esa carta
+console.log("Tarjeta actualizada con nuevo color:", card);
 
+return card;
+}
 
-module.exports = changeColor
+module.exports = changeColor;
