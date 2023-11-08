dec1e96cfa6c3110b7c6023f9117d9edbbac3c46
RobertoLayna <potter.laynar@gmail.com>
2023-11-07 19:39:50 -0500

    Reverse

--- modules/change-color.js
+++ modules/change-color.js
@@ -1,15 +1,17 @@
 
-function changeColor(/* carta */, /* colores */) {
+function changeColor(/* carta */ /* colores */) {
     // Recibir carta
     // preguntarle al usuario que color escoger
     // cambiar el color 
     // Regresar la carta con la actualizacion 
     // del color seleccionado
+    console.log("Impresion de prueba")
 }
 // Modificar este codigo para que reciba el arreglo de colores y 
 // los muestre como opciones
 // el color seleccionado debe ser el nuevo valor para 
 // card.color, regresar esa carta
+changeColor()
 
 
 module.exports = changeColor
--- modules/reverse.js
+++ modules/reverse.js
@@ -1,13 +1,29 @@
-function cardReverse() {
-  if (players < 2) {
-    console.log("El juego requiere al menos 2 jugadores.");
-    return;
+// Obtenemos al jugador actual mediante la llamada a la funcion
+// y nuestra bandera "isReverse" que nos da un true
+// si esta activa la reversa y un false si esta en giro normal
+function cardReverse(totalPlayers, actualPlayer, isReverse) {
+  const indexPlayer = parseInt(actualPlayer.split("_")[1])
+  console.log(indexPlayer)
+  let nextPlayer = null
+  //Si el ciclo esta en reveresar
+  //regresar el sig jugador como el jugador anterior
+  if (isReverse) {
+    if (indexPlayer == 0) {
+      nextPlayer = totalPlayers-1
+    } else {
+      nextPlayer = indexPlayer-1
+    }
+    
+  } else {
+    if (indexPlayer == totalPlayers-1) {
+      nextPlayer = 0
+    } else {
+      nextPlayer = indexPlayer+1
+    }
   }
-
-  // Cambia el orden de los jugadores invirtiendo el sentido del juego
-  currentPlayer = (currentPlayer - 1 + players) % players;
-
-  console.log(`El jugador ${currentPlayer} ha jugado una carta reversa. El sentido del juego se invierte.`);
+  return {actualPlayer, nextPlayer: `player_${nextPlayer}`, isReverse}
 }
+
+console.log(cardReverse(4, "player_3", false))
 
 module.exports = cardReverse
